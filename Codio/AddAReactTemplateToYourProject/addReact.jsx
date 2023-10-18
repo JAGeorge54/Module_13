5db52a9eb56f8bd625006b716df049f611f790a5
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2023-10-18 14:59:20 -0400

    added react dom container

--- Codio/AddAReactTemplateToYourProject/addReact.jsx
+++ Codio/AddAReactTemplateToYourProject/addReact.jsx
@@ -20,3 +20,6 @@ class LikeButton extends React.Component {
     );
   }
 }
+// ... the starter code you pasted ...
+
+const domContainer = document.querySelector('#like_button_container');ReactDOM.render(e(LikeButton), domContainer);
