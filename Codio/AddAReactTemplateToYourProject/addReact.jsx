cf1b920e476bea4c18de0f3f085177c9626da22e
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2023-10-18 14:53:35 -0400

    added starter code

---
+++ Codio/AddAReactTemplateToYourProject/addReact.jsx
@@ -0,0 +1,22 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { id: "like-button", onClick: () => this.setState({ liked: true }) , id: 'like-button'},
+      'Like'
+    );
+  }
+}
