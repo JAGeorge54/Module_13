b24306d88966a60df25e4c7fe47cd70371647fbd
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2023-10-23 14:34:46 -0400

    added new code

--- Follow Along/reactFromManaged/index.js
+++ Follow Along/reactFromManaged/index.js
@@ -1,35 +1,30 @@
 function SignUp() {
-    const [year, setYear ] =  React.useState('Freshman');
-    const [name, setName ] =  React.useState('');
-    const [email, setEmail ] =  React.useState('');
-    const [password, setPassword ] =  React.useState('');
-    const [remember, setRemember ] =  React.useState('');
+    const [values, handleChange] = useForm({year:'',name:'', email:'', password:'', checkbox:false});
 
     function handle(){
-        console.log('Year:', year);
-        console.log('Name:', name);
-        console.log('Email:', email);
-        console.log('Password:', password);
-        console.log('Remembered:', remember);
+        console.log('values:', values);
     }
+
     return (
         <>
-        <select value={year} onChange={e => setYear(e.target.value)}>
+        <select name="year" value={values.year} onChange={handleChange}>
             <option>Freshman</option>
             <option>Sophmore</option>
             <option>Junior</option>
             <option>Senior</option>
         </select>
         <div>Name</div>
-        <input value={name} onChange={e => setName(e.target.value)}/>
+        <input type="text" name="name" value={values.name} onChange={handleChange}/>
         <div>Email</div>
-        <input value={email} onChange={e => setEmail(e.target.value)}/>
+        <input type="text" name="email" value={values.email} onChange={handleChange}/>
         <div>Password</div>
-        <input value={password} onChange={e => setPassword(e.target.value)}/>
-        <div><input type="checkbox" value={remember} onChange={e => setRemember(e.target.checked)}/>Remember me</div>
+        <input type="text" name="password" value={values.password} onChange={handleChange}/>
+        <div><input type="checkbox" name="checkbox" value={values.checkbox} onChange={handleChange}/>
+            Remember me
+        </div>
         <button onClick={handle}>Submit</button>
         </>
-    )
+    );
 }
 
 ReactDOM.render(
