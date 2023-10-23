589be8f7725805271ddb3e6a2f796bd221b655e3
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2023-10-23 17:14:15 -0400

    added name validation

--- Follow Along/validateForm/index.js
+++ Follow Along/validateForm/index.js
@@ -1,18 +1,31 @@
 function SignUp(){
     const [values, handleChange] = useForm({year:'',name:'',email:'',password:'',checkbox:false});
-    
+    const [errors, setError] = React.useState({nameError:'', emailError:'', passwordError:''});
+
+    function validate() {
+        if (!values.name) {
+            setError({...errors, nameError: 'bad'})
+        }
+        else {
+            setError({...errors, nameError: ''})
+        }
+    }
+
     function handle(){
-        console.log('values:',values);
+        validate();
     }
 
     return (
         <>
         <div>Name</div>
         <input type="text" name="name" value={values.name} onChange={handleChange}/>
+        <div style={{color:'red'}}>{errors.nameError}</div>
         <div>Email</div>
         <input type="text" name="email" value={values.email} onChange={handleChange}/>
+        <div style={{color:'red'}}>{errors.emailError}</div>
         <div>Password</div>
         <input type="text" name="password" value={values.password} onChange={handleChange}/>
+        <div style={{color:'red'}}>{errors.passwordError}</div>
         <br />
         <button onClick={handle}>Submit</button>    
         </>
